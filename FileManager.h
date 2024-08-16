75d13ab0c754ed7ac207bd4f49d81d3343040c89
Derrity <derrity0731@gmail.com>
2024-08-16 15:50:05 +0800

    move function getNumberOfChunks() from private to public

--- FileManager.h
+++ FileManager.h
@@ -11,11 +11,10 @@ private:
     std::string filePath;
     std::size_t chunkSize;
 
-    std::size_t getNumberOfChunks();
-
 public:
     FileManager(const std::string& _filePath,const std::size_t &_chunkSize);
 
+    size_t getNumberOfChunks();
     void splitFile();
     void mergeFiles();
     void deleteChunks();
